1ad9df565beeda6bbd4210842440683ebc72c45c
ViniiGarcia <viniciusgsgarcia@hotmail.com>
2023-11-22 14:42:15 -0300

    atualização

---
+++ NivelHeroi.js
@@ -0,0 +1,59 @@
+// Classificando Nivel do Heroi
+let nomeHeroi = ("Zé da Manga")
+let valorXP = 6451
+let i = valorXP
+while (i<=1000) {
+    console.log (nomeHeroi + "-Nivel Ferro")
+    i++
+    break
+
+}
+//Nivel Bronze
+i = valorXP
+while (i >1000<=2000) {
+    console.log (nomeHeroi + "-Nivel Bronze")
+    i++
+    break
+}
+//Nivel Prata
+i = valorXP
+while (i >2000<=5000) {
+    console.log (nomeHeroi + "-Nivel Prata")
+    i++
+    break
+}
+//Nivel Ouro
+i = valorXP
+while (i >6000<=7000) {
+    console.log (nomeHeroi + "-Nivel Ouro")
+    i++
+    break
+}
+//Nivel Platina
+i = valorXP
+while (i >7000<=8000) {
+    console.log (nomeHeroi + "-Nivel Platina")
+    i++
+    break
+}
+//Nivel Ascendente
+i = valorXP
+while (i >8000<=9000) {
+    console.log (nomeHeroi + "-Nivel Ascendente")
+    i++
+    break
+}
+//Nivel Imortal
+i = valorXP
+while (i >9000<=10000) {
+    console.log (nomeHeroi + "-Nivel Imortal")
+    i++
+    break
+}
+//Nivel Radiante
+i = valorXP
+while (i >10000) {
+    console.log (nomeHeroi + "-Nivel Imortal")
+    i++
+    break
+}
