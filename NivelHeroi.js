045aab79f12ae15db44c6ed60042232469ce3d6a
ViniiGarcia <viniciusgsgarcia@hotmail.com>
2023-11-22 16:30:50 -0300

    Agora Vai

--- NivelHeroi.js
+++ NivelHeroi.js
@@ -1,59 +1,42 @@
 // Classificando Nivel do Heroi
 let nomeHeroi = ("Zé da Manga")
-let valorXP = 6451
-let i = valorXP
-while (i<=1000) {
-    console.log (nomeHeroi + "-Nivel Ferro")
-    i++
-    break
-
+let valorXP = 10001
+if (valorXP <= 1000){
+    console.log(nomeHeroi + "-Nivel Ferro")
 }
-//Nivel Bronze
-i = valorXP
-while (i >1000<=2000) {
-    console.log (nomeHeroi + "-Nivel Bronze")
-    i++
-    break
-}
-//Nivel Prata
-i = valorXP
-while (i >2000<=5000) {
-    console.log (nomeHeroi + "-Nivel Prata")
-    i++
-    break
-}
-//Nivel Ouro
-i = valorXP
-while (i >6000<=7000) {
-    console.log (nomeHeroi + "-Nivel Ouro")
-    i++
-    break
-}
-//Nivel Platina
-i = valorXP
-while (i >7000<=8000) {
-    console.log (nomeHeroi + "-Nivel Platina")
-    i++
-    break
-}
-//Nivel Ascendente
-i = valorXP
-while (i >8000<=9000) {
-    console.log (nomeHeroi + "-Nivel Ascendente")
-    i++
-    break
-}
-//Nivel Imortal
-i = valorXP
-while (i >9000<=10000) {
-    console.log (nomeHeroi + "-Nivel Imortal")
-    i++
-    break
-}
-//Nivel Radiante
-i = valorXP
-while (i >10000) {
-    console.log (nomeHeroi + "-Nivel Imortal")
-    i++
-    break
-}
+else {
+    if (valorXP <= 2000){
+        console.log(nomeHeroi + "-Nivel Bronze")
+    }
+    else{
+        if (valorXP <= 5000){
+        console.log(nomeHeroi + "-Nivel Prata")
+        }  
+         else{
+            if(valorXP <= 7000){
+                console.log (nomeHeroi + "-Nivel Ouro")
+            }
+            else{
+                if(valorXP <= 8000){
+                    console.log (nomeHeroi + "-Nivel Platina")
+                }
+                else{
+                    if(valorXP <= 9000){
+                        console.log (nomeHeroi + "-Nivel Ascendente")
+                    }
+                    else{
+                        if(valorXP <= 10000){
+                            console.log (nomeHeroi + "-Nivel Imortal")
+                        }
+                        else{
+                            if(valorXP > 10000){
+                                console.log(nomeHeroi + "-Nivel Radiante")
+                            }
+                        }
+                    }
+                    
+                }
+            }
+         } 
+    }
+}   
